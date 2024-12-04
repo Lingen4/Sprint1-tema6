a65f0b97a834e120f3feea46c931aaabbc72189b
Lingen4 <lucrfb@gmail.com>
2024-12-04 21:23:42 +0100

    Magic constants

--- action.php
+++ action.php
@@ -2,8 +2,8 @@
 session_start();
 
 $name = htmlspecialchars($_POST['name']);
-$email = $_POST['email'];
-$username = $_POST['username'];
+$email = htmlspecialchars($_POST['email']);
+$username = htmlspecialchars($_POST['username']);
 
 $_SESSION['name']= $_POST['name'];
 $_SESSION['email']=$_POST['email'];
@@ -14,11 +14,14 @@ $datos = "Estos datos son los de ". $name ."<br>";
 $datos.= "Su e-mail es ".$email."<br>";
 $datos.= "Su nombre de usuario es ".$username."<br>";
 
-echo($datos)."<br>";
+echo($datos)."<br> Este archivo actualmente se encuentra en--> ".__FILE__;
 
-echo "Persona en la sesión actual--> ". $_SESSION['name']."<br>";
-echo "E-mail del usuario actual--> ".$_SESSION['email']."<br>";
-echo "Nombre de usuario actual--> ".$_SESSION['username']."<br>";
+echo "<br>Lo siguiente está en la linea ". __LINE__ ."<h4>Persona en la sesión actual--> ". $_SESSION['name']."</h4>";
+echo "<h4>E-mail del usuario actual--> ".$_SESSION['email']."</h4>";
+echo "<h4>Nombre de usuario actual--> ".$_SESSION['username']."</h4>";
+
+
+
 
 
 //header('Location:formulario.html');
